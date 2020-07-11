f4e166b9bb1b79b7a2e08a3d0a897e02d1180d85
Ryan Lee <ryanlee7132@gmail.com>
2020-07-11 19:20:24 -0400

    Compensation inputs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Box, Grid, TextField } from "@material-ui/core";
+import {
+  LineChart,
+  Line,
+  BarChart,
+  PieChart,
+  ScatterChart,
+  AreaChart,
+} from "recharts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box component="span" m={1}>
+      <img src={require("./logo.png")} alt="Logo" />
+
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Box>
+          {/* Add Salary  */}
+          {/* Main 2 Columns */} {/* Main Column 1 */}
+          <con
+            container
+            d
+            justify="center"
+            ali
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            {/* Column 1 Subcolumn */}
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+            >
+              <TextField
+                id="filled-number"
+                type="number"
+                label="Base Salary (Yearly)"
+                variant="filled"
+                color="secondary"
+              ></TextField>
+              <TextField
+                id="filled-number"
+                type="number"
+                label="Target Bonus"
+                variant="filled"
+              ></TextField>
+              <TextField
+                id="filled-number"
+                type="number"
+                label="Stock Award (4 Years)"
+                variant="filled"
+              ></TextField>
+              <TextField
+                id="filled-number"
+                type="number"
+                label="Signing Bonus"
+                variant="filled"
+              ></TextField>
+            </Grid>
+          </con>
+          {/* Column 1 Subcolumn */}
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          ></Grid>
+        </Box>
+
+        {/* Main Column 2 */}
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+        ></Grid>
+      </Grid>
+    </Box>
   );
 }
 
